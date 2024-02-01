891c3d3fdb1c1544e08ade1b6735423f2e62c992
Ade Oke <ade.oke@digital.hmrc.gov.uk>
2024-02-01 11:27:17 +0000

    regression runner added

---
+++ src/test/scala/uk/gov/hmrc/test/ui/cucumber/runner/RegressionRunner.scala
@@ -0,0 +1,21 @@
+package uk.gov.hmrc.test.ui.cucumber.runner
+
+import io.cucumber.junit.{Cucumber, CucumberOptions}
+import org.junit.runner.RunWith
+
+@RunWith(classOf[Cucumber])
+@CucumberOptions(
+  features = Array("src/test/resources/features"),
+  glue = Array("uk.gov.hmrc.test.ui.cucumber.stepdefs"),
+  plugin = Array(
+    "pretty",
+    "html:target/cucumber",
+    "json:target/cucumber.json",
+    "junit:target/test-reports/homepagerunner.xml"
+  ),
+  tags = "@regression"
+)
+class RegressionRunner {
+
+}
+
