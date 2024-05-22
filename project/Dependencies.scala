dc9f3630d37cb4ffa419c69107462d16f2746acc
PaulCDurham <23658502+PaulCDurham@users.noreply.github.com>
2024-05-22 11:31:42 +0100

    HIPP-835: Update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,14 +5,14 @@ object Dependencies {
   val test = Seq(
     "com.novocode"          % "junit-interface"        % "0.11"     % Test,
     "com.typesafe"          % "config"                 % "1.4.3"    % Test,
-    "io.cucumber"           % "cucumber-junit"         % "7.15.0"   % Test,
-    "io.cucumber"           % "cucumber-picocontainer" % "7.11.0"   % Test,
-    "io.cucumber"          %% "cucumber-scala"         % "8.20.0"   % Test,
-    "io.github.etspaceman" %% "scalacheck-faker"       % "8.0.2"    % Test,
-    "io.rest-assured"       % "scala-support"          % "5.3.2"    % Test,
+    "io.cucumber"           % "cucumber-junit"         % "7.18.0"   % Test,
+    "io.cucumber"           % "cucumber-picocontainer" % "7.18.0"   % Test,
+    "io.cucumber"          %% "cucumber-scala"         % "8.23.0"   % Test,
+    "io.github.etspaceman" %% "scalacheck-faker"       % "8.0.4"    % Test,
+    "io.rest-assured"       % "scala-support"          % "5.4.0"    % Test,
     "junit"                 % "junit"                  % "4.13.2"   % Test,
     "org.scalatest"        %% "scalatest"              % "3.2.18"   % Test,
     "org.scalatestplus"    %% "selenium-4-2"           % "3.2.13.0" % Test,
-    "uk.gov.hmrc"          %% "ui-test-runner"         % "0.29.0"   % Test
+    "uk.gov.hmrc"          %% "ui-test-runner"         % "0.30.0"   % Test
   )
 }
